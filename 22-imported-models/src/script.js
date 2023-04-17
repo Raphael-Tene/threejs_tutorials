ac862b138061a19e211c9775f304563fbb8ba435
raphael <teneraphael@gmail.com>
2023-04-17 19:53:02 +0000

    finished the lessons

---
+++ 22-imported-models/src/script.js
@@ -0,0 +1,167 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "lil-gui";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Models
+const gltfLoader = new GLTFLoader();
+
+gltfLoader.load("/models/Fox/glTF/Fox.gltf", (gltf) => {
+  // if the model has one scene, use
+  // scene.add(gltf.scene);
+  // use while loop to iterate through all the children of the gltf.scene if you have multiple models in the gltf file
+  //   while (gltf.scene.children.length > 0) {
+  //     scene.add(gltf.scene.children[0]);
+  //   }
+
+  // you can also duplicate the model by using the spread operator
+  //   scene.add(...gltf.scene.children);
+  // scale the gltf model if it is too big
+  gltf.scene.scale.set(0.025, 0.025, 0.025);
+  // create animation mixer
+  const mixer = new THREE.AnimationMixer(gltf.scene);
+  // create animation action
+  const action = mixer.clipAction(gltf.animations[1]);
+  action.play();
+  // create a clock
+  const clock = new THREE.Clock();
+  let previousTime = 0;
+  // create a tick function
+  const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+    const deltaTime = elapsedTime - previousTime;
+    previousTime = elapsedTime;
+    // update the mixer
+    mixer.update(deltaTime);
+    // call tick again on the next frame
+    window.requestAnimationFrame(tick);
+  };
+  tick();
+  scene.add(gltf.scene);
+});
+
+// draco files are more lightweight than gltf files
+// let's load one
+// !Re-watch the tutorial to see how to use draco files and adding workers
+// gltfLoader.setDRACOLoader(new DRACOLoader());
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: "#444444",
+    metalness: 0,
+    roughness: 0.5,
+  })
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(2, 2, 2);
+scene.add(camera);
+
+// Controls
+// @ts-ignore
+const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 0.75, 0);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  // @ts-ignore
+  canvas: canvas,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
